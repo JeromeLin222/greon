fc6745d64abe3b84b60fe5e2abda7e1689574b4f
billtll <130990963+billtll@users.noreply.github.com>
2025-09-09 20:15:26 +0800

    [MODIFY] 圖片名稱 [ADD] CollectFilmItem components 設定 [ADD] CollectFilmList page 設定

---
+++ src/components/CollectFilmItem.jsx
@@ -0,0 +1,10 @@
+import collect from "../assets/images/icons/collect.svg";
+import collectWhite from "../assets/images/icons/collect-white.svg";
+
+import "./CollectFilmItem.css";
+
+function CollectFilmItem() {
+  return <li></li>;
+}
+
+export default CollectFilmItem;
--- src/pages/CollectFilmList.jsx
+++ src/pages/CollectFilmList.jsx
@@ -1,4 +1,73 @@
+import collect1 from "../assets/images/Personal_Page_Search_Result/VideoThumbnail-1.svg";
+import collect2 from "../assets/images/Personal_Page_Search_Result/VideoThumbnail-2.svg";
+import collect3 from "../assets/images/Personal_Page_Search_Result/VideoThumbnail-3.svg";
+import collect4 from "../assets/images/Personal_Page_Search_Result/VideoThumbnail-4.svg";
+import collect5 from "../assets/images/Personal_Page_Search_Result/VideoThumbnail-5.svg";
+import collect6 from "../assets/images/Personal_Page_Search_Result/VideoThumbnail-6.svg";
+import collect7 from "../assets/images/Personal_Page_Search_Result/VideoThumbnail-7.svg";
+
+import "./CollectFilmList.css";
+
 function CollectFilmList() {
+  const collectFilm = [
+    {
+      id: 1,
+      img: collect1,
+      alt: "collect1",
+      title: "地球力：傾聽來自荒野的呼喚",
+      episode: 8,
+      isCollected: false,
+    },
+    {
+      id: 2,
+      img: collect2,
+      alt: "collect2",
+      title: "與牠們的約定：守護瀕危生命的行動詩",
+      episode: 8,
+      isCollected: false,
+    },
+    {
+      id: 3,
+      img: collect3,
+      alt: "collect3",
+      title: "森林之心：維繫生命的綠色網絡",
+      episode: 8,
+      isCollected: false,
+    },
+    {
+      id: 4,
+      img: collect4,
+      alt: "collect4",
+      title: "城市與野境的邊界：尋找共存的智慧",
+      episode: 8,
+      isCollected: false,
+    },
+    {
+      id: 5,
+      img: collect5,
+      alt: "collect5",
+      title: "從一片葉到一片林：種下未來的希望",
+      episode: 8,
+      isCollected: false,
+    },
+    {
+      id: 6,
+      img: collect6,
+      alt: "collect6",
+      title: "寂靜的守護者：為無法言語的生命發聲",
+      episode: 8,
+      isCollected: false,
+    },
+    {
+      id: 7,
+      img: collect7,
+      alt: "collect7",
+      title: "海洋的微光：從珊瑚礁到鯨豚的生命故事",
+      episode: 8,
+      isCollected: false,
+    },
+  ];
+
   return (
     <>
       <div className="text-h6 text-info-500 text-center mt-app-header">
